abf5fd464b8346798d1b54e853690fb32f2828c6
xiyuan_fengyu <xiyuan_fengyu@163.com>
2017-03-31 15:16:54 +0800

    initial commit

--- src/main/java/com/xiyuan/helloworld/task/HelloWorld.java
+++ src/main/java/com/xiyuan/helloworld/task/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.xiyuan.helloworld.task;
 
 import com.google.gson.JsonElement;
+import com.xiyuan.spider.annotation.OnMessage;
 import com.xiyuan.spider.annotation.OnStart;
+import com.xiyuan.spider.annotation.OnTime;
 import com.xiyuan.spider.annotation.Task;
 
 /**
@@ -10,7 +12,9 @@ import com.xiyuan.spider.annotation.Task;
 @Task
 public class HelloWorld {
 
-    @OnStart(name = "hello world", url = "https://www.baidu.com", js = "js/HelloWorld.js")
+    @OnStart(name = "hello world", url = "https://www.baidu.com", js = "js/HelloWorld.js", timeout = 30)
+    @OnTime(name = "hello world", cron = "*/10 * * * * ? ", url = "https://www.baidu.com", js = "js/HelloWorld.js")
+    @OnMessage(name = "hello world", fromQueue = "just test", js = "js/HelloWorld.js", parallel = 10)
     public void test(String url, JsonElement result) {
         System.out.println(result);
     }
